e09784d70dbfb2c9de071d4c9e312b3039d4ef90
mirabeev <mirabeev@bc.edu>
2025-03-18 12:01:39 -0400

    changed course/team creation name

--- BrainNotFound/urls.py
+++ BrainNotFound/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
     path('peer_answer_quant/', views.peer_answer_quant, name='peer_answer_quant'),
     path('peer_answer_qual/', views.peer_answer_qual, name='peer_answer_qual'),
-    path('course_team_creation/', views.course_team_creation, name='course_team_creation'),
+    path('create/', views.create, name='create'),
     path('', views.landing_page, name="landing"),
 
     path('accounts/', include('allauth.urls')),
--- PeerConnect/views.py
+++ PeerConnect/views.py
@@ -11,8 +11,8 @@ def peer_answer_qual(request):
 def peer_answer_quant(request):
     return render(request, "PeerConnect/peer_answer_quant.html", {})
 
-def course_team_creation(request):
-    return render(request, "PeerConnect/course_team_creation.html", {})
+def create(request):
+    return render(request, "PeerConnect/create.html", {})
 
 def landing_page(request):
     return render(request, "PeerConnect/landingpage.html", {})
