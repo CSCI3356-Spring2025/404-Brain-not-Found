d77f13849cd4eddeb1049560631c5d35d913558b
Priyal Shah <112434337+priyals0@users.noreply.github.com>
2025-03-23 13:35:44 -0400

    added course and team models

--- PeerConnect/models.py
+++ PeerConnect/models.py
@@ -4,7 +4,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    admin = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+    is_professor = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
+    
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    professor = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='courses_taught', limit_choices_to={'is_professor': True})
+    students = models.ManyToManyField(UserProfile, related_name='courses_enrolled', limit_choices_to={'is_student': True})
+
+    def __str__(self):
+        return self.name
+    
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='teams')
+    members = models.ManyToManyField(UserProfile, related_name='teams', limit_choices_to={'is_student': True})
+    
+    def __str__(self):
+        return f"{self.name} ({self.course.name})"
+
--- PeerConnect/views.py
+++ PeerConnect/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import UserProfile
+from .models import UserProfile, Course, Team
 
 # Create your views here.
 def student_dashboard(request):
     #user_profile = get_object_or_404(UserProfile, user=request.user)
     user_profile, created = UserProfile.objects.get_or_create(user=request.user)
-    if not user_profile.admin:
+    if not user_profile.is_professor:
         user_type = "Student"
     else:
         user_type = "Professor"
