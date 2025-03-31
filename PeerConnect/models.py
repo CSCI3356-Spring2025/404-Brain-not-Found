ce6d80fa3310beb146ba1a9a29d3a8ca29db6aa3
mirabeev <mirabeev@bc.edu>
2025-03-30 22:48:49 -0400

    Adding likert/open response to Question model

--- PeerConnect/models.py
+++ PeerConnect/models.py
@@ -52,10 +52,15 @@ class Assessment(models.Model):
 
 
 class Question(models.Model):
+    QUESTION_TYPE_CHOICES = [
+        ('likert', 'Likert Scale'),
+        ('open', 'Open-Ended'),
+    ]
+    
     assessment = models.ForeignKey(Assessment, related_name='questions', on_delete=models.CASCADE)
     text = models.TextField()
     order = models.PositiveIntegerField()
-
+    type = models.CharField(max_length=10, choices = QUESTION_TYPE_CHOICES, default = "open")
     def __str__(self):
-        return f"Question {self.order}: {self.text[:30]}"
+        return f"{self.get_question_type_display()} Question {self.order}: {self.text[:30]}"
 
