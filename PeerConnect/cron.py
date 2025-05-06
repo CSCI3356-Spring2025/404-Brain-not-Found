bdae4156f2ee5d90c656a73a67b1b4c8fcfc3245
mirabeev <mirabeev@bc.edu>
2025-05-06 17:46:38 -0400

    Commented out debugging

--- PeerConnect/cron.py
+++ PeerConnect/cron.py
@@ -7,21 +7,21 @@ from datetime import timedelta
 
     # Reminds students when an assessment becomes available to edit
 def remind_available():
-    print("Running Available Assessment job")
+    #print("Running Available Assessment job")
     now = timezone.now()
 
         # filters for unpublished assessments with availble dates earlier than now
     upcoming_assessments = Assessment.objects.filter(due_date__gte = now, available_date__lte = now, published=False)
-    print(f"Found {upcoming_assessments.count()} available_to_edit assessments.")
+    #print(f"Found {upcoming_assessments.count()} available_to_edit assessments.")
 
     for assessment in upcoming_assessments:
-        print(f"Looking at {assessment.name}, sent = {assessment.available_reminder_sent}")
+        #print(f"Looking at {assessment.name}, sent = {assessment.available_reminder_sent}")
 
         if not assessment.available_reminder_sent: # only send available email once
-            print(f"{assessment.name} available reminder not sent ")
+            #print(f"{assessment.name} available reminder not sent ")
             for course in assessment.course.all():
                 for student in course.students.all():
-                    print("Sending email to", student.user.email)
+                    #print("Sending email to", student.user.email)
                     user = student.user
                     send_mail(
                         subject=f"Reminder: Peer Assessment Available to Edit",
@@ -38,40 +38,40 @@ def remind_available():
                     )
             assessment.available_reminder_sent = True
             assessment.save()
-        else:
-            print("Available reminder already sent")
+        #else:
+            #print("Available reminder already sent")
 
 
 def remind_unsubmitted():
-    print("Running reminder job")
+    #print("Running reminder job")
     now = timezone.now()
-    print("Now:", now)
+    #print("Now:", now)
 
         # gets date/time 12 hours from now
     twelve_hrs_later = now + timedelta(hours=24)
-    print("Twelve hours later:", twelve_hrs_later)
+    #print("Twelve hours later:", twelve_hrs_later)
 
         # filters due_dates <= 12 hrs away and >= now
     upcoming_assessments = Assessment.objects.filter(due_date__lte=now + timezone.timedelta(hours=24), due_date__gte=now, published=False)
 
-    print(f"Found {upcoming_assessments.count()} upcoming assessments.")
+    #print(f"Found {upcoming_assessments.count()} upcoming assessments.")
     
     for assessment in upcoming_assessments:
         if not assessment.due_soon_reminder_sent:
 
             for course in assessment.course.all():
-                print(f"Course: {course.name}, students: {course.students.count()}")
+                #print(f"Course: {course.name}, students: {course.students.count()}")
                 for student in course.students.all():
-                    print(f"Checking student: {student.user.email}")
+                    #print(f"Checking student: {student.user.email}")
                     has_responded = QuestionResponse.objects.filter(
                         assessment=assessment,
                         student=student
                     ).exists()
 
-                    print(f"Has responded: {has_responded}")
+                    #print(f"Has responded: {has_responded}")
 
                     if not has_responded:
-                        print("Sending email to", student.user.email)
+                        #print("Sending email to", student.user.email)
                         user = student.user
                         send_mail(
                             subject=f"Reminder: Peer Assessment Due in 12 Hours",
@@ -88,5 +88,5 @@ def remind_unsubmitted():
                         )
             assessment.due_soon_reminder_sent = True
             assessment.save()
-        else:
-            print("Assessment reminder already sent")
+        #else:
+            #print("Assessment reminder already sent")
