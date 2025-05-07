02fb24c08125bf0a12750be6fa52793eb5165539
lmarfeo <marfeol@bc.edu>
2025-05-07 16:41:25 -0400

    adding installed_apps

--- BrainNotFound/settings.py
+++ BrainNotFound/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
