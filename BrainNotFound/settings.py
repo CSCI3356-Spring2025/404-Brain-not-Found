be31b268a28701c8cc1048981d479db6c663fc53
lmarfeo <marfeol@bc.edu>
2025-05-07 16:11:24 -0400

    ready to deploy

--- BrainNotFound/settings.py
+++ BrainNotFound/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mj_gukr95a$$1pt8ydutuu%pumhhtdy%3_oh0_4k$(pz+1xv7f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -87,6 +88,13 @@ import os
 # Add this if not already in your settings.py
 STATIC_URL = '/static/'
 
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Ensure Django knows where to find static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "PeerConnect", "static"),  # Ensure this path is correct
@@ -98,11 +106,22 @@ WSGI_APPLICATION = 'BrainNotFound.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+##DATABASES = {
+##    'default': {
+##       'ENGINE': 'django.db.backends.sqlite3',
+ ##       'NAME': BASE_DIR / 'db.sqlite3',
+ ##   }
+##}
+
+import dj_database_url
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
